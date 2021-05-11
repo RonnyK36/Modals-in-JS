b69e9c8b5a7d8a809bcc99f9f3831d9baef1802e
RonnyK36 <ronnykelvyne3@gmail.com>
2021-05-11 23:08:00 +0300

    Major changes, worked on SCRIPT.JS added functionality

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+const btnHideModal = document.querySelector('.close-modal');
+
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsShowModal.length; i++)
+    btnsShowModal[i].addEventListener('click', openModal);
+
+btnHideModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
